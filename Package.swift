da3dc2df5c0454355619b069c4126ccb1c63fee9
Juan Armario Munoz <juan.armario.munoz@gmail.com>
2025-11-24 15:30:10 -0500

    Update Package.swift for versioning and URL

--- Package.swift
+++ Package.swift
@@ -3,9 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "NoibuSessionReplay",
-    platforms: [
-        .iOS(.v16)
-    ],
+    platforms: [.iOS(.v16)],
     products: [
         .library(
             name: "NoibuSessionReplay",
@@ -15,9 +13,10 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "NoibuSessionReplay",
-            url: "https://github.com/Noibu/session-replay-ios_test/releases/download/v0.1.0/NoibuSessionReplay.xcframework.zip",
+            url: "https://github.com/Noibu/session-replay-ios_test/releases/download/0.1.0/NoibuSessionReplay.xcframework.zip",
             checksum: "b5c9e3e2624c7289f6a8b53c5a13a79442af4626f9a786502dbb8df18a9d0770"
         )
     ]
 )
+
 
