315aab04fd5facd611c3c27daf54faa4132f71a7
Juan Armario Munoz <juan.armario.munoz@gmail.com>
2025-12-11 13:40:18 -0500

    Update NoibuSessionReplay to version 0.6.0
    Updated the binary target URL and checksum for NoibuSessionReplay.

--- Package.swift
+++ Package.swift
@@ -13,11 +13,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "NoibuSessionReplay",
-            url: "https://github.com/Noibu/session-replay-ios_test/releases/download/0.5.0/NoibuSessionReplay.xcframework.zip",
-            checksum: "2c0c8664c6d501e03d7385b26bd05c1af43f5cce5257c035690aa949ad1d34f6"
+            url: "https://github.com/Noibu/session-replay-ios_test/releases/download/0.6.0/NoibuSessionReplay.xcframework.zip",
+            checksum: "a8d0db2e89cabda5fde7be8a49ec61c0fa4ab93eb9022f0eab876792efa35f4c"
         )
     ]
 )
+
 
 
 
