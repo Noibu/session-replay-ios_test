28e3dfc09402673a79b014fecf36bc072ad942d9
Juan Armario Munoz <juan.armario.munoz@gmail.com>
2025-12-15 14:03:10 -0500

    Modify Package.swift to update binary target checksum
    Updated the checksum for the NoibuSessionReplay binary target.

--- Package.swift
+++ Package.swift
@@ -14,10 +14,11 @@ let package = Package(
         .binaryTarget(
             name: "NoibuSessionReplay",
             url: "https://github.com/Noibu/session-replay-ios_test/releases/download/0.8.0/NoibuSessionReplay.xcframework.zip",
-            checksum: "a7c7e06eeda61a87ac2b6a4c87c3b43122a5ec23ffe9ba0061f1e16c96e48dcd"
+            checksum: "1aa48e4d90bcfca2ecc48984a0e9a6b834be4423ff63c10d919ad047a1affee6"
         )
     ]
 )
+
 
 
 
