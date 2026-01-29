2d9d87e542d7fa8b8844491b6b141f5ae5cb613a
Juan Armario Munoz <juan.armario.munoz@gmail.com>
2026-01-29 12:19:46 -0500

    Fix URL for NoibuSessionReplay binary target
    Updated the URL for the NoibuSessionReplay binary target.

--- Package.swift
+++ Package.swift
@@ -13,11 +13,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "NoibuSessionReplay",
-            url: "https://github.com/Noibu/session-replay-ios_test/releases/download/0.1.0/NoibuSessionReplay.xcframework.zip",
+            url: "https://github.com/Noibu/session-replay-ios/releases/download/0.1.0/NoibuSessionReplay.xcframework.zip",
             checksum: "82f796273f748389eb60ef3f6e32bc5684fe39d9610ed797396ea9241f69db89"
         )
     ]
 )
+
 
 
 
