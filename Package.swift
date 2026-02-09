c10b2c9acf617f57e15f279e8a84c9aa13a65c9e
Juan Armario Munoz <juan.armario.munoz@gmail.com>
2026-02-09 11:41:56 -0500

    Update NoibuSessionReplay binary target checksum
    Updated the checksum for the NoibuSessionReplay binary target.

--- Package.swift
+++ Package.swift
@@ -14,10 +14,11 @@ let package = Package(
         .binaryTarget(
             name: "NoibuSessionReplay",
             url: "https://github.com/Noibu/session-replay-ios/releases/download/0.1.0/NoibuSessionReplay.xcframework.zip",
-            checksum: "1fc36b66909442ddcfb69ea3a3edd989997c745e221912bce9eb2853f48e4d26"
+            checksum: "dad0059525eeee8a0e13884ea32159b4547d150401419f054edaec7158788dd7"
         )
     ]
 )
+
 
 
 
